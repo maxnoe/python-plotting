658ca1d0478cdeb6c7f00b8c91b6f1e19b64e63c
Maximilian Noethe <maximilian.noethe@udo.edu>
2014-07-14 22:17:47 +0200

    changed output to png

--- legend_fill_between.py
+++ legend_fill_between.py
@@ -50,4 +50,4 @@ ax.set_ylim(-1.1, 1.1)
 
 # always call tight_layout before saving ;)
 fig.tight_layout()
-fig.savefig("legend_fill_between.pdf", dpi=300)
+fig.savefig("legend_fill_between.png", dpi=300)
