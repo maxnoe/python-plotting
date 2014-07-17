1c349fb545500cbfdd3364622d6625803b4d1943
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2014-07-17 12:16:47 +0200

    create_pi_labels: added directly changing ticks

--- source/create_pi_labels.py
+++ source/create_pi_labels.py
@@ -3,7 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 from fractions import Fraction
 
-def create_pi_labels(a=0, b=2, step=0.5):
+
+
+def create_pi_labels(a=0, b=2, step=0.5, ax=None, direction='x'):
+    """
+    A function that gives back ticks an labels in radians
+    
+    Keyword arguments:
+    a -- lower limit is a*pi (default 0.0)
+    b -- upper limit is b*pi (default 2.0)
+    step -- step is step*pi  (default 0.5)
+    ax -- if ax is not None then ticks and labels are set for this axes (default None)
+    direction -- 'x' or 'y' or 'z' (default 'x') which axis you want to label
+    
+    return value is ticks, labels 
+    """
+    
     max_denominator = int(1/step)
     values = np.arange(a, b+0.1*step, step)
     fracs = [Fraction(x).limit_denominator(max_denominator) for x in values]
@@ -28,6 +43,19 @@ def create_pi_labels(a=0, b=2, step=0.5):
                 labels.append(r"${}\pi$".format(frac.numerator))
             else:
                 labels.append(r"$\frac{{{}}}{{{}}} \pi$".format(frac.numerator, frac.denominator))
+                
+    if ax is not None:
+        if direction == 'x':
+            ax.set_xticks(ticks)
+            ax.set_xticklabels(labels)
+        elif direction == 'y':
+            ax.set_yticks(ticks)
+            ax.set_yticklabels(labels)
+        elif direction == 'z':
+            ax.set_zticks(ticks)
+            ax.set_zticklabels(labels)
+        else:
+            print("direction ", direction, "is not a proper argument")
 
     return ticks, labels
 
@@ -47,11 +75,7 @@ ax.set_ylim(-1.1, 1.1)
 ax.set_xlabel(r"$x$")
 ax.set_ylabel(r"$f(x)$")
 
-
-
-ticks, labels = create_pi_labels(-1, 2, 1/3)
-ax.set_xticks(ticks)
-ax.set_xticklabels(labels)
+create_pi_labels(-1, 2, 1/3, ax, 'x')
 
 ax.legend(loc="best")
 
